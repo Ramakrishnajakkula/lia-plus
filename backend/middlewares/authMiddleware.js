b120218872943871d524f68366fe1703f5daf602
Ramakrishnajakkula <ramanjaneyuluj14@gmail.com>
2025-06-26 08:08:52 +0530

    k

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,7 +1,6 @@
-// Change from CommonJS export to ES module export
 const jwt = require('jsonwebtoken');
 
-function authMiddleware(req, res, next) {
+module.exports = function (req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(401).json({ message: 'No token provided' });
@@ -14,6 +13,4 @@ function authMiddleware(req, res, next) {
     } catch (err) {
         return res.status(401).json({ message: 'Invalid or expired token' });
     }
-}
-
-export default authMiddleware;
+};
